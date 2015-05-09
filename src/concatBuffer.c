8bab06e25805da72df20b21c2ac22e84ced208c6
kaoru <kaoru@kaoru-Inspiron-N4030>
2015-05-09 20:44:07 -0300

    proceso con comentarios

--- src/concatBuffer.c
+++ src/concatBuffer.c
@@ -7,9 +7,9 @@ extern size_t strLength(char* c);
 void concatBuffer(char** line, char* buffer) {
     size_t len1 = *line ? strlen(*line) : 0;
     size_t len2 = buffer ? strlen(buffer) : 0;
-    char* concat = realloc(*line, len1 + len2 + 1);
+    char* concat = realloc(*line, len1 + len2 + 1); //reserva mas espacio para line, ¿por que mas 1?
     if (concat) {
-        memcpy(concat + len1, buffer, len2 + 1);
+        memcpy(concat + len1, buffer, len2 + 1); //copia los elementos de buffer a partir del ultimo de line
         *line = concat;
     }
 }
--- src/tacFile.c
+++ src/tacFile.c
@@ -10,21 +10,21 @@ int tacFile(FILE* fp) {
     const int bufIncrSize = 10;
     char ** arrayLines = NULL;
     int lineCounter = 0;
-    char buffer[bufIncrSize];
-    char* line = (char *) calloc(bufIncrSize, sizeof (char));
-    while (fgets(buffer, bufIncrSize, fp)) {
-        concatBuffer(&line, buffer);
+    char buffer[bufIncrSize]; //buffer es un array de chars de tamaño 10
+    char* line = (char *) calloc(bufIncrSize, sizeof (char)); //line es un puntero a 10 chars
+    while (fgets(buffer, bufIncrSize, fp)) { //lee del archivo de a 10 caracteres o hasta que encuentre fin de linea
+        concatBuffer(&line, buffer); //le concatena a line el contenido del buffer
         char lastCharacterBuffer = buffer[strlen(buffer) - 1];
         if (isEndOfLine(lastCharacterBuffer)) {
-        	//printf("%c %s",lastCharacterBuffer," es un salto de linea");
             arrayLines = realloc(arrayLines,
-                    (lineCounter + 1) * sizeof (char *));
-            arrayLines[lineCounter] = line;
+                    (lineCounter + 1) * sizeof (char *)); //reserva espacio para una linea mas en arrayLines
+            arrayLines[lineCounter] = line; //guarda line en el array
             lineCounter++;
-            line = (char *) calloc(bufIncrSize, sizeof (char));
+            line = (char *) calloc(bufIncrSize, sizeof (char)); //reinicializa line (seteando la memoria en 0)
         }
     }
 
+    //esto podriamos sacarlo a otro archivo .S
     int i;
     for (i = lineCounter - 1; i >= 0; i--) {
         printf("%s", arrayLines[i]);

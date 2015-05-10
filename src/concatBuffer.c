e7a1fb859c7e899e6a5e346a3aa7899ffa14e53b
kaoru <kaoru@kaoru-Inspiron-N4030>
2015-05-10 18:13:04 -0300

    concatBuffer.S (sin terminar)

--- src/concatBuffer.c
+++ src/concatBuffer.c
@@ -5,13 +5,22 @@
 extern size_t strLength(char* c);
 
 char* concatBuffer(char* line, char* buffer) {
-    size_t len1 = line ? strLength(line) : 0;
-    size_t len2 = buffer ? strLength(buffer) : 0;
-    char* concat = realloc(line, len1 + len2 + 1); //reserva mas espacio para line, ¿por que mas 1?
-    if (concat) {
-        memcpy(concat + len1, buffer, len2 + 1); //copia los elementos de buffer a partir del ultimo de line
-        return concat;
-    }
-    return line;
+	size_t len1 = 0;
+	if (line) {
+		len1 = strLength(line);
+	}
+
+	size_t len2 = 0;
+	if (buffer) {
+		len2 = strLength(buffer);
+	}
+
+	if (len2 == 0) {
+		return line;
+	}
+
+	char* concat = realloc(line, len1 + len2 + 1); //reserva mas espacio para line, ¿por que mas 1?
+    memcpy(concat + len1, buffer, len2 + 1); //copia los elementos de buffer a partir del ultimo de line
+    return concat;
 }
 

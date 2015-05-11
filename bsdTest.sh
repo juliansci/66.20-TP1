96d8a345bf4934f97e432db18302bdb1a71345c3
Julian Scialabba <julian.scialabba@gmail.com>
2015-05-10 23:07:05 -0300

    Script de test para Netbsd

--- bsdTest.sh
+++ bsdTest.sh
@@ -1,26 +1,8 @@
-#!/bin/bash
-
 testFile() {
 	echo -e "\nTEST "$1".txt"
 	./tp1 test-files/$1.txt | ./tp1 > output/$1.txt
-	res1=`md5 test-files/$1.txt`
-	res2=`md5 output/$1.txt`
-	
-	echo "md5 de $1: $res2"
-	if [ $res1 == $res2 ]; 
-	then
-		echo -e "OK\n"
-	else
-		echo -e "ERROR\n"
-	fi
-}
-
-testWords() {
-	echo -e "\nTEST WORDS"
-	res1=`cat '/usr/share/dict/words' | md5`
-	res2=`./tp1 '/usr/share/dict/words' | ./tp1 | md5`
-	
-	echo "md5 de words: $res2"
+	res1=`cat test-files/$1.txt | md5`
+	res2=`cat output/$1.txt | md5`
 	if [ $res1 == $res2 ]; 
 	then
 		echo -e "OK\n"
@@ -30,7 +12,7 @@ testWords() {
 }
 
 echo Compilando...
-gcc -o tp1 main.c
+make
 
 testFile empty
 testFile basic
@@ -38,5 +20,3 @@ testFile empty-lines
 testFile large-file
 testFile return
 testFile status
-
-testWords

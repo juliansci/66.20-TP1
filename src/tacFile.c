e7030c94d0d94c03aa39305aa3bfb621f78c6ab1
Julian Scialabba <julian.scialabba@gmail.com>
2015-05-24 12:08:18 -0300

    Corrijo errores menores de tacFile.c y pruebo liberacion de char** con myfree.

--- src/tacFile.c
+++ src/tacFile.c
@@ -13,8 +13,10 @@ int tacFile(FILE* fp) {
     int lineCounter = 0;
     char ** arrayLines = (char**) mymalloc((lineCounter + 1) * sizeof (char *));
     char buffer[bufIncrSize]; //buffer es un array de chars de tamaño 10
-    char* line = (char*) mymalloc(1 * sizeof (char *));
+    char* line = (char*) mymalloc(1);
     *line = 0;
+    char** prueba = (char**) mymalloc((10) * sizeof (char*));
+    myfree(prueba);
 
     while (fgets(buffer, bufIncrSize, fp)) { //lee del archivo de a 10 caracteres o hasta que encuentre fin de linea
         char *newLine = concatBuffer(line, buffer); //le concatena a line el contenido del buffer
@@ -28,14 +30,14 @@ int tacFile(FILE* fp) {
             //  myfree(oldArrayLines);
             arrayLines[lineCounter] = line; //guarda line en el array
             lineCounter++;
-            line = (char*) mymalloc(1 * sizeof (char *));
+            line = (char*) mymalloc(1);
             *line = 0;
         }
     }
 
     printLines(lineCounter, arrayLines);
     myfree(line);
-  //  myfree(arrayLines);
+    // myfree(arrayLines);
 
     return (EXIT_SUCCESS);
 }

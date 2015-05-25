2ae54dd26e57c697cd4eb8e91e882ed43066512d
kaoru <kaoru@kaoru-Inspiron-N4030>
2015-05-25 14:39:51 -0300

    readFile.S (pero llama a fgets)

--- src/tacFile.c
+++ src/tacFile.c
@@ -9,6 +9,7 @@ extern void *mymalloc(size_t);
 extern void myfree(void *);
 extern char** resizeArrayLines(char** arrayLines, int arraySize);
 extern char** storeNewLine(char lastCharacterBuffer, int* lineCounter, char ** arrayLines, char** line);
+extern char *readFile(char *buffer, int n, FILE *stream);
 
 
 int tacFile(FILE* fp) {
@@ -18,12 +19,12 @@ int tacFile(FILE* fp) {
     char buffer[bufIncrSize]; //buffer es un array de chars de tamaño 10
     char* line = (char*) mymalloc(1);
     *line = 0;
-    while (fgets(buffer, bufIncrSize, fp)) { //lee del archivo de a 10 caracteres o hasta que encuentre fin de linea
+    while (readFile(buffer, bufIncrSize, fp)) { //lee del archivo de a 10 caracteres o hasta que encuentre fin de linea
         char *newLine = concatBuffer(line, buffer); //le concatena a line el contenido del buffer
         myfree(line);
         line = newLine;
         char lastCharacterBuffer = buffer[strlen(buffer) - 1];
-	arrayLines = storeNewLine(lastCharacterBuffer,&lineCounter,arrayLines,&line); 
+        arrayLines = storeNewLine(lastCharacterBuffer,&lineCounter,arrayLines,&line);
     }
 
     printLines(lineCounter, arrayLines);
